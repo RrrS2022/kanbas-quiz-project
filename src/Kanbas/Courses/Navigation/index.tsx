6801ec6bfed00eb64737d9d4d782abc4d830d6f0
RrrS2022 <sun.yuhu@northeastern.edu>
2024-04-13 13:41:55 -0400

    update course nav bar

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,13 +1,41 @@
+// import { Link, useLocation } from "react-router-dom";
+// import "./index.css";
+// function CourseNavigation() {
+//   const links = ["Home", "Modules", "Piazza", "Grades", "Assignments"];
+//   const { pathname } = useLocation();
+//   return (
+//     <ul className="wd-navigation">
+//       {links.map((link, index) => (
+//         <li key={index} className={pathname.includes(link) ? "wd-active" : ""}>
+//           <Link to={link}>{link}</Link>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
+// export default CourseNavigation;
 import { Link, useLocation } from "react-router-dom";
-import "./index.css";
+import "./index.css"; // feel free to use the CSS from previous assignments
+import { FaEyeSlash } from "react-icons/fa";
 function CourseNavigation() {
-  const links = ["Home", "Modules", "Piazza", "Grades", "Assignments"];
+  const links = ["Home", "Modules", "Piazza", "Zoom Meetings", "Assignments", 
+    "Quizzes", "Grades",  "People", "Panopto Video", "Discussion", "Announcement",
+    "Pages", "Files", "Rubrics", "Outcomes", "Collaborations", "Syllabus", "Settings"];
+
+  const linksWithEyeSlash = ["Discussion", "Announcement", "Pages", "Files", "Rubrics",
+      "Outcomes", "Collabrations", "Syllabus"];
+
   const { pathname } = useLocation();
   return (
     <ul className="wd-navigation">
       {links.map((link, index) => (
         <li key={index} className={pathname.includes(link) ? "wd-active" : ""}>
           <Link to={link}>{link}</Link>
+          {linksWithEyeSlash.includes(link) && (
+            <span className="float-end" style={{ marginLeft: "auto" }}>
+              <FaEyeSlash />
+            </span>
+          )}
         </li>
       ))}
     </ul>
---
+++ src/Kanbas/Courses/Quiz/Details/index.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { FaCheckCircle, FaPencilAlt, FaEllipsisV} from "react-icons/fa";
+
+function QuizDetails() {
+
+    return(
+        <div className="ms-5 ,me-5">
+            <div className="d-flex justify-content-end">
+                <span style={{ color: 'green' }}><FaCheckCircle />Published</span>
+                <button 
+                    className="btn btn-light"
+                    style={{marginLeft:"5px", marginRight:"10px", borderRadius:"4px"}}>
+                        Preview
+                </button>
+                <button className="btn btn-light"
+                    style={{ marginRight:"5px", borderRadius:"4px"}}>
+                    <FaPencilAlt />Edit
+                </button>
+                <button className="btn btn-light"
+                    style={{borderRadius:"4px"}}>
+                    <FaEllipsisV />
+                </button>
+            </div>
+            <hr />
+            <div>
+                <h1>Q1-HTML</h1>
+                <div style={{textAlign:"center"}}>
+                    Quiz Type  Graded Quiz 
+                </div>
+                <div style={{textAlign:"center"}}>
+                    Points  the sum of the points of all questions in the quiz
+                </div>
+                <div style={{textAlign:"center"}}>
+                    Assignment Group  Quizzes
+                </div>
+                <div style={{textAlign:"center"}}>
+                    Shuffle Answers No
+                </div>
+                <div style={{textAlign:"center"}}>
+                    Time Limit 30 Minutes
+                </div>
+
+                <div style={{textAlign:"center"}}>
+                    Multiple Attempts No
+                </div>
+                <div style={{textAlign:"center"}}>
+                    View Responses Always
+                </div>
+                <div style={{textAlign:"center"}}>
+                    Show Correct Answers Immediately
+                </div>
+                <div style={{textAlign:"center"}}>
+                    One Question at a Time Yes
+                </div>
+                <div style={{textAlign:"center"}}>
+                    Require Respondus LockDown Browser No
+                </div>
+                <div style={{textAlign:"center"}}>
+                    Required to View Quiz Results No
+                </div>
+                <div style={{textAlign:"center"}}>
+                    Webcam Required No
+                </div>
+                <div style={{textAlign:"center"}}>
+                    Lock Questions After Answering No
+                </div>
+
+                <div className="d-flex">
+                    
+
+                </div>
+
+            </div>
+        
+        </div>
+    )
+}
