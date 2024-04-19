3ce79a6fa7ff3e86bbda5d9f51ea041581134384
stephanieP0826 <stephanie@firstprinciplespublishing.com>
2024-04-19 12:10:33 -0400

    add details

--- src/Kanbas/Courses/Quiz/quizzesReducer.tsx
+++ src/Kanbas/Courses/Quiz/quizzesReducer.tsx
@@ -5,7 +5,8 @@ interface Quiz {
   _id?: string;
   title: string;
   description: string;
-  points: string;
+  points: number;
+  questionNumber: number;
   dueDate: string;
   availableFromDate: string;
   availableUntilDate: string;
@@ -18,47 +19,50 @@ interface QuizzesState {
 }
 
 const initialState: QuizzesState = {
-    quizzes: [],
-    quiz: { title: "New Quiz", description: "this is the new quiz", 
-    points: "100", dueDate: "03/15/2024", 
-    availableFromDate: "02/15/2024",  availableUntilDate: "03/15/2024", published: true},
+  quizzes: [],
+  quiz: {
+    title: "New Quiz", description: "this is the new quiz",
+    points: 100, questionNumber: 10, dueDate: "2024-05-10",
+    availableFromDate: "2024-02-15", availableUntilDate: "2024-05-15",
+    published: true
+},
   };
 
-  const quizzesSlice = createSlice({
-    name: "quizzes",
-    initialState,
-    reducers: {
-      addQuiz: (state, action) => {
-        state.quizzes = [action.payload, ...state.quizzes];
-      },
-  
-    deleteQuiz: (state, action) => {
-        state.quizzes = state.quizzes.filter(
-          (quiz) => quiz._id !== action.payload
-        );
-      },
-      updateQuiz: (state, action) => {
-        state.quizzes = state.quizzes.map((quiz) => {
-          if (quiz._id === action.payload._id) {
-            return action.payload;
-          } else {
-            return quiz;
-          }
-        });
-      },
-      selectQuiz: (state, action) => {
-        state.quiz = action.payload;
-      },
-      setQuiz: (state, action) => {
-        state.quizzes = action.payload;
-      },
-  
+const quizzesSlice = createSlice({
+  name: "quizzes",
+  initialState,
+  reducers: {
+    addQuiz: (state, action) => {
+      state.quizzes = [action.payload, ...state.quizzes];
     },
-  });
 
-  export const { addQuiz, deleteQuiz,
-    updateQuiz, selectQuiz, setQuiz } = quizzesSlice.actions;
-  export default quizzesSlice.reducer;
+    deleteQuiz: (state, action) => {
+      state.quizzes = state.quizzes.filter(
+        (quiz) => quiz._id !== action.payload
+      );
+    },
+    updateQuiz: (state, action) => {
+      const index = state.quizzes.findIndex(quiz => quiz._id === action.payload._id);
+      if (index !== -1) {
+        state.quizzes[index] = {
+          ...state.quizzes[index],
+          ...action.payload.changes,
+        };
+      }
+    },
+    selectQuiz: (state, action) => {
+      state.quiz = action.payload;
+    },
+    setQuiz: (state, action) => {
+      state.quizzes = action.payload;
+    },
+
+  },
+});
+
+export const { addQuiz, deleteQuiz,
+  updateQuiz, selectQuiz, setQuiz } = quizzesSlice.actions;
+export default quizzesSlice.reducer;
 
 
 
