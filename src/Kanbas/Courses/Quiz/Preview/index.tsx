30dc5288bab66ca419af7712139abb04cae2d959
stephanieP0826 <stephanie@firstprinciplespublishing.com>
2024-04-22 01:17:22 -0400

    Preview screen

--- src/Kanbas/Courses/Quiz/Preview/index.tsx
+++ src/Kanbas/Courses/Quiz/Preview/index.tsx
@@ -1,93 +1,92 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { KanbasState } from "../../../store";
-import { FiAlertCircle , FiArrowRight } from "react-icons/fi";
+import { FiAlertCircle, FiArrowRight } from "react-icons/fi";
 import { MdOutlineEdit } from "react-icons/md";
-import "../../Assignments/index.css"
+import "../../Assignments/index.css";
+import { setQuestions } from "../Questions/questionsReducer";
+import { findAllQuestionsForQuiz } from "../Questions/client";
 
-export default function Preview () {
+export default function Preview() {
+    const { quizId } = useParams();
+    const dispatch = useDispatch();
     const quiz = useSelector((state: KanbasState) => 
         state.quizzesReducer.quiz);
-    const questionItem = useSelector((state: KanbasState) =>
-        state.questionReducer.question);
+    const questions = useSelector((state: KanbasState) =>
+        state.questionReducer.questions); // This should be 'questions', not 'question'
+
+    useEffect(() => {
+        if (quizId) {
+            findAllQuestionsForQuiz(quizId)
+                .then(questions => {
+                    dispatch(setQuestions(questions)); // Update the Redux store with the fetched questions
+                })
+                .catch(error => {
+                    console.error("Failed to fetch questions", error);
+                });
+        }
+    }, [quizId, dispatch]);
+
     return (
         <div>
             <h1>Quiz Preview Screen</h1>
             <h2>{quiz.title}</h2>
-            <div style={{color:"#BC3518", backgroundColor: "#F6CEC5", height: "35px",
-                display: "flex", alignItems: "center", }}>
-                <p style={{marginTop: "15px"}}>
-                    <FiAlertCircle /> This is a preview of the publishd version of the quiz
+            <div style={{
+                color: "#BC3518", backgroundColor: "#F6CEC5", height: "35px",
+                display: "flex", alignItems: "center",
+            }}>
+                <p style={{ marginTop: "15px" }}>
+                    <FiAlertCircle /> This is a preview of the published version of the quiz
                 </p>
             </div>
             <br />
             <div>
-                {/** should create the getTime const later, placeholder for now */}
-                <p>Started: time</p>
+                <p>Started: time</p> {/* Placeholder for actual time */}
             </div>
             <h1>Quiz Instructions</h1>
             <hr />
-            <div className="container" style={{border: "1px solid #ccc", margin: 0, padding: 0}}>
-                <div 
-                    style={{ display: "flex", justifyContent: "space-between", alignItems: "center", 
-                    backgroundColor: "lightgrey", margin: 0, padding: 10}}>
-                    <b> Question 1 </b> {/* placeholder for actual question number */}
-                    <p style={{ float: "right", margin: 0 }}> 1 pts </p>
+            {questions.map((question, index) => (
+                <div key={question._id} className="container" style={{ border: "1px solid #ccc", margin: 0, padding: 0 }}>
+                    <div style={{
+                        display: "flex", justifyContent: "space-between", alignItems: "center",
+                        backgroundColor: "lightgrey", margin: 0, padding: 10
+                    }}>
+                        <b> Question {index + 1} </b>
+                        <p style={{ float: "right", margin: 0 }}> {question.points} pts </p>
+                    </div>
+                    <div style={{ margin: 0, padding: 10 }}>
+                        {question.question}
+                        <hr />
+                        <div style={{ padding: 0 }}>
+                            {question.options.map((option: any, idx: any) => (
+                                <React.Fragment key={idx}>
+                                    <input type="radio" name={`question_${index}`} value={option} />
+                                    &#160; {option}
+                                    <hr />
+                                </React.Fragment>
+                            ))}
+                        </div>
+                    </div>
                 </div>
-                <div style={{margin:0, padding: 10}}>
-                    {/** This is the placeholder for the actual question display */}
-                    An HTML label element can be associated with an HTML input element by setting their id 
-                    attributes to the same value. The resulting effect is that when you click on the label 
-                    text, the input element receives focus as if you had click on the input element itself
-                    {questionItem && <div>{questionItem.question}</div>}
-
-                    <hr />
-                    <div style={{padding:0}}>
-                        <input  type="radio" 
-                                />
-                        &#160;possible choice1<hr /> 
-                        <input type="radio" 
-                                />
-                        &#160;possible choice2<hr />
-                        <input type="radio" 
-                                />
-                        &#160;possible choice3<hr />
-                        <input type="radio" 
-                                />
-                        &#160;possible choice4<hr />
-                        <input type="radio" 
-                                />
-                        &#160;possible choice5<br />
-                    </div>    
-                </div>
-            </div>
-            <div style={{padding: 0}}>
+            ))}
+            <div style={{ padding: 0 }}>
                 <div style={{ display: "flex", justifyContent: "flex-end", alignItems: "center" }}>
                     <button className="btn btn-light" style={{ float: "right", margin: "20px", border: "solid #ccc" }}>
                         Next <FiArrowRight />
                     </button>
-                
                 </div>
-
-                <div className="form-group d-flex" 
-                    style={{  border: "1px solid #ccc", height: "60px", display: "flex",  alignItems: "center" }}>
-                    <div style={{ flex: 1 }}></div> 
-                    <p style={{marginTop: "15px", color: "grey"}}> Quiz saved at time </p>
-                    <button className="btn btn-light" 
-                        style={{height: "40px", float: "right", border: "solid #ccc",
-                            marginRight: "10px", marginLeft: "10px"}}>
+                <div className="form-group d-flex" style={{ border: "1px solid #ccc", height: "60px", display: "flex", alignItems: "center" }}>
+                    <div style={{ flex: 1 }}></div>
+                    <p style={{ marginTop: "15px", color: "grey" }}> Quiz saved at time </p>
+                    <button className="btn btn-light" style={{
+                        height: "40px", float: "right", border: "solid #ccc",
+                        marginRight: "10px", marginLeft: "10px"
+                    }}>
                         Submit Quiz
                     </button>
                 </div>
-                <div className="form-group d-flex" 
-                    style={{  border: "1px solid #ccc", height: "30px",  
-                        backgroundColor: "lightgray", display: "flex",  alignItems: "center",
-                        margin: "20px", padding: 0}}>
-                    <MdOutlineEdit />&#160; Keep Editing This Quiz
-                </div>
             </div>
-            
-
         </div>
-    )
+    );
 }
